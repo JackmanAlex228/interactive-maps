63b30aa05e35da77d8c5b7f00653ec742e78925b
Alex Jackman <jackman.alex.w@gmail.com>
2021-10-04 15:09:10 -0600

    started using bootstrap react, been getting module parse error

--- maps-app/src/components/Selector.js
+++ maps-app/src/components/Selector.js
@@ -1,4 +1,6 @@
 import React, { useRef, useLayoutEffect } from "react";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Selector = (props) => {
 
@@ -13,11 +15,17 @@ const Selector = (props) => {
 
     return (
         <div>
-            <select className='mapSelector' style={ selectorStyle } name='maps' id='maps'>
+            <DropdownButton id="dropdown-basic-button" title="Change Map">
+                <Dropdown.Item>World Happiness</Dropdown.Item>
+                <Dropdown.Item>Water Per Capita</Dropdown.Item>
+                <Dropdown.Item>Average Population</Dropdown.Item>
+                <Dropdown.Item active='false' disabled='true'>More maps coming!</Dropdown.Item>
+            </DropdownButton>
+            {/* <select className='mapSelector' style={ selectorStyle } name='maps' id='maps'>
                 <option>World Happiness</option>
                 <option>Water Per Capita</option>
                 <option>Average Population Density</option>
-            </select>
+            </select> */}
             {/* <h1 style={ selectorStyle }>This is the selector</h1> */}
         </div>
     )
